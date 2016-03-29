4586259468fa42f82940b08d7fc03e6247c4425c
Anton Vidovic <anton.vidovic@gmx.de>
2016-03-29 18:40:03 +0200

    Refactor insert-char to use char2chtype

--- source/insch.lisp
+++ source/insch.lisp
@@ -10,19 +10,7 @@ cursor is not changed.
 If the destination coordinates y and x are given, move the cursor
 there first."
   (let ((winptr (.winptr window))
-        (chtype (logior (typecase char
-                          ;; if the char is already an integer chtype
-                          (integer char)
-                          ;; alternative chars are given as keywords
-                          (keyword (acs char))
-                          ;; if it is a lisp char, convert it to an integer first
-                          (character (char-code char))
-                          (t 0))
-                        ;; convert the pair to an integer, then bit shift it by 8
-                        (if color-pair (ash (pair->number color-pair) 8) 0)
-                        ;; the attribute bitmasks already are bitshifted to the
-                        ;; correct position in the chtype
-                        (if attributes (attrs2chtype attributes) 0))))
+        (chtype (char2chtype char attributes color-pair)))
     (cond ((and y x)
            (%mvwinsch winptr y x chtype))
           (t
