17356d819e7440881af9d1b438fbb4c3ca22de42
Anton Vidovic <anton.vidovic@gmx.de>
2017-03-04 17:02:27 +0100

    Refactored and completed adding and extracting of wide chars
    %wadd-wch will not be used any more because it cant display colors.
    Maybe in future after we have complete cffi type translators.
    
    Wide chars are displayed by encoding them as UTF-8 first and then
    adding them chtype by chtype with attributes and colors.

--- source/add_wch.lisp
+++ source/add_wch.lisp
@@ -1,6 +1,6 @@
 (in-package :de.anvi.croatoan)
 
-(defun add-wide-char (window char &key attributes color-pair y x)
+(defun add-wide-char (window char &key attributes color-pair y x n)
   "Add the wide (multi-byte) char to the window, then advance the cursor.
 
 If the destination coordinates y and x are given, move the cursor to the
@@ -9,31 +9,22 @@ destination first and then add the character.
 If n is given, write n chars. If n is -1, as many chars will be added
 as will fit on the line."
   (when (and y x) (move window y x))
-  (let ((winptr (.winptr window))
-        ;; this does work as expected for %waddch but doesnt work for %wadd-wch.
-        (attr (char2chtype 0 attributes color-pair)))
-    (with-foreign-object (ptr '(:struct cchar_t))
-      (setf ptr (convert-to-foreign (list 'cchar-attr attr 'cchar-chars (char-code char)) ; Lisp plist to C struct.
-                                    '(:struct cchar)))      
-      (%wadd-wch winptr ptr))))
-
-#|
-      ;; debug used in t03d
-      (move window 4 0)
-      (format window "~b ~x~%" attr attr)
-|#
-
-(defun add-wide-char-utf-8 (window char &key attributes color-pair y x)
-  "Add the wide (multi-byte) char to the window, then advance the cursor.
-
-If the destination coordinates y and x are given, move the cursor to the
-destination first and then add the character.
-
-If n is given, write n chars. If n is -1, as many chars will be added
-as will fit on the line."
-  (when (and y x) (move window y x))
-  (mapc #'(lambda (ch) (add-char window ch :attributes attributes :color-pair color-pair))
-        (unicode-to-utf-8 char)))
-
-;; TODO: n, see add-char
-;;       proper type translation cchar_t->xchar
+  (let ((count  (if n
+                   (if (= n -1)
+                       (- (.width window) (cadr (.cursor-position window)))
+                       n)
+                   1))
+        (code-point (typecase char
+                      (integer char)
+                      (character (char-code char)))))
+    (typecase char
+      (complex-char
+       ;; if we have a complex char, use its attributes and colors.
+       (loop repeat count do
+            (mapc #'(lambda (ch) (add-char window ch :attributes (.attributes char) :color-pair (.color-pair char)))
+                  (unicode-to-utf-8 (char-code (.simple-char char))))))
+      ;; if we have a lisp char or an integer, use the attributes and colors passed as arguments.
+      (t
+       (loop repeat count do
+            (mapc #'(lambda (ch) (add-char window ch :attributes attributes :color-pair color-pair))
+                  (unicode-to-utf-8 code-point)))))))
--- source/in_wch.lisp
+++ source/in_wch.lisp
@@ -3,29 +3,23 @@
 (defun extract-wide-char (window &key y x)
   "Extract and return a wide character (code > 255) from the window.
 
-If the destination coordinates y and x are given, move the cursor
-to the destination first."
+If the destination coordinates y and x are given, first move the cursor."
   (when (and y x) (move window y x))
-
-  (with-foreign-object (ptr '(:struct cchar_t)) ; we absolutely need cchar_t here, cchar crashes sbcl.
+  ;; we absolutely need cchar_t here, cchar crashes sbcl.
+  (with-foreign-object (ptr '(:struct cchar_t))
     ;; read a struct cchar_t into the space allocated with ptr
     (%win-wch (.winptr window) ptr)
-    ;; for testing purposes, print the struct cchar_t pointed to by ptr
-    ;;(%wadd-wch (.winptr window) ptr)))
     ;; when we use cchar, the int is dereferenced, cchar_t returns a pointer to the wchar_t array.
     ;; convert-from-foreign returns a plist, then we read the char from the plist using getf
-    (getf (convert-from-foreign ptr '(:struct cchar)) 'cchar-chars) ))
+    (let* ((plist (convert-from-foreign ptr '(:struct cchar)))
+           (char (getf plist 'cchar-chars))
+           (attr (getf plist 'cchar-attr)))
+      (make-instance 'complex-char
+                     :simple-char (code-char char)
+                     :attributes (loop for i in *valid-attributes* if (logtest attr (get-bitmask i)) collect i)
+                     ;; first get the color attribute bits by log-AND-ing them with ch.
+                     ;; then right shift them by 8 to extract the color int from them.
+                     ;; then get the color pair (:white :black) associated with that number.
+                     :color-pair (number->pair (ash (logand attr (get-bitmask :color)) -8))))))
 
-#|
-
-;;TODO: extract the attributes, then construct and return a complex-char
-;;(getf (convert-from-foreign ptr '(:struct cchar)) 'cchar-attr) ))
-(make-instance 'complex-char
-               :simple-char __char__
-               :attributes (loop for i in *valid-attributes* if (logtest __attr__ (get-bitmask i)) collect i)
-               ;; first get the color attribute bits by log-AND-ing them with ch.
-               ;; then right shift them by 8 to extract the color int from them.
-               ;; then get the color pair (:white :black) associated with that number.
-               :color-pair (number->pair (ash (logand __attr__ (get-bitmask :color)) -8)))
-
-|#
+;; http://stackoverflow.com/questions/30198550/extracting-wide-chars-w-attributes-in-ncurses
