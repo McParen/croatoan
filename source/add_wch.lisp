02b2852777521331692ce7f259ba69f50e88f7b0
Anton Vidovic <anton.vidovic@gmx.de>
2017-09-07 20:24:19 +0200

    Refactor add-wide-char and its helper function
    Use more general funcall-with-cchar_t instead of specific add-cchar_t.
    
    We then can use the same helper function with echo-wide-char and
    insert-wide-char.

--- source/add_wch.lisp
+++ source/add_wch.lisp
@@ -29,6 +29,24 @@ as will fit on the line."
             (mapc #'(lambda (ch) (add-char window ch :attributes attributes :color-pair color-pair))
                   (unicode-to-utf-8 code-point)))))))
 
+;; we can use this for add-wide-char, echo-wide-char, insert-wide-char
+(defun funcall-with-cchar_t (fn winptr char attr_t color-pair-number count)
+  "Create a cchar_t and apply function fn to winptr and cchar_t.
+
+If optional parameter count is not given, perform the action one single time.
+
+cchar_t is a C struct representing a wide complex-char in ncurses.
+
+This function is a wrapper around %setcchar and should not be used elsewhere."
+  (with-foreign-objects ((ptr '(:struct cchar_t))
+                         (wch 'wchar_t 5))
+    (dotimes (i 5) (setf (mem-aref wch 'wchar_t i) 0))
+    (setf (mem-aref wch 'wchar_t) char)
+    (%setcchar ptr wch attr_t color-pair-number (null-pointer))
+    (if (= count 1)
+        (funcall fn winptr ptr)
+        (dotimes (i count) (funcall fn winptr ptr)) )))
+
 (defun add-wide-char (window char &key attributes color-pair y x n)
   "Add the wide (multi-byte) char to the window, then advance the cursor.
 
@@ -48,24 +66,12 @@ as will fit on the line."
                    1)))
     (typecase char
       ;; if we have a lisp char or an integer, use the attributes and colors passed as arguments.
-      (integer (loop repeat count do (add-cchar_t winptr char attr color-pair-number)))
-      (character (loop repeat count do (add-cchar_t winptr (char-code char) attr color-pair-number)))
+      (integer      (funcall-with-cchar_t #'%wadd-wch winptr char             attr color-pair-number count))
+      (character    (funcall-with-cchar_t #'%wadd-wch winptr (char-code char) attr color-pair-number count))
       ;; if we have a complex char, use its own attributes and colors.
-      (complex-char (loop repeat count do
-                         (add-cchar_t winptr
-                                      (char-code (.simple-char char))
-                                      (if (.attributes char) (attrs2chtype (.attributes char)) 0)
-                                      (if (.color-pair char) (pair->number (.color-pair char)) 0)))))))
-
-(defun add-cchar_t (winptr char attr_t color-pair-number)
-  "Create and display a cchar_t to the window.
-
-cchar_t is a C struct representing a wide complex-char in ncurses.
-
-This function is a wrapper around %setcchar and should not be used elsewhere."
-  (with-foreign-objects ((ptr '(:struct cchar_t))
-                         (wch 'wchar_t 5))
-    (dotimes (i 5) (setf (mem-aref wch 'wchar_t i) 0))
-    (setf (mem-aref wch 'wchar_t) char)
-    (%setcchar ptr wch attr_t color-pair-number (null-pointer))
-    (%wadd-wch winptr ptr)))
+      (complex-char (funcall-with-cchar_t #'%wadd-wch
+                                          winptr
+                                          (char-code (.simple-char char))
+                                          (if (.attributes char) (attrs2chtype (.attributes char)) 0)
+                                          (if (.color-pair char) (pair->number (.color-pair char)) 0)
+                                          count)))))
