0c5d26d9122f1175772066ba00cdfa12a54f38ce
Daniel Vedder <d.vedder@web.de>
2019-01-01 12:38:17 +0100

    Various bug fixes in the shape functions

--- source/shape.lisp
+++ source/shape.lisp
@@ -36,18 +36,20 @@
 			:documentation "The complex character to use for plotting"))
 	(:documentation "A shape is a list of coordinates, relative to an origin, that can be plotted in a window."))
 
-(defmethod shape-extent ((shape shape))
-	"Return min-y, min-x, max-y, and max-x of a shape's coordinates"
-	;;TODO
-	)
+(defun inbounds-p (coordinate win)
+	"Test whether the given coordinate is within the given window"
+	(not (or (minusp (first coordinate))
+			 (minusp (second coordinate))
+			 (>= (first coordinate) (.height win))
+			 (>= (second coordinate) (.width win)))))
 
-(defmethod draw-shape ((shape shape) (win window) &optional (char complex-char))
-	(let ((y0 (.y-origin shape)) (x0 (.x-origin shape))
-			 (c (if char char (.plot-char shape))))
+(defmethod draw-shape ((shape shape) (win window))
+	(with-accessors ((y0 .y-origin) (x0 .x-origin) (c .plot-char)) shape
 		(dolist (coord (.coordinates shape))
-			(add-char win (.simple-char c)
-				:y (+ y0 (first coord)) :x (+ x0 (second coord))
-				:attributes (.attributes c) :color-pair (.color-pair c)))))
+			(when (inbounds-p coord win) ;;xxx needs adjusted coordinates!
+				(add-char win (.simple-char c)
+					:y (+ y0 (first coord)) :x (+ x0 (second coord))
+					:attributes (.attributes c) :color-pair (.color-pair c))))))
 
 (defmethod delete-shape ((shape shape) (win window) &optional (bg-col :black))
 	 "A utility function to delete a shape by drawing over it in black"
@@ -89,30 +91,37 @@
 ;;             shape.append((x,y))
 ;;     return shape
 
-(defun line (y0 x0 y1 x1 &key char)
+(defun line2 (y0 x0 y1 x1 &key char)
 	"Return a straight line between two points"
 	(when (> x0 x1)
 		;;reverse the positions to ensure a positive slope
 		;;XXX is this really necessary?
 		(setf zx x0 zy y0)
 		(setf x1 x0 y1 y0 x0 zx y0 zy))
-	(do* ((l (make-instance 'shape :char char))
+	(do* ((l (make-instance 'shape))
 			 (coords nil)
 			 (slope (/ (- y1 y0) (- x1 x0)))
 			 (steep-p (> (abs slope) 1))
 			 (p (if steep-p y0 x0) (1+ p))
 			 (q (round (- (if steep-p x1 y1)
+						   (* slope (- (if steep-p y1 x1) p))))
+				 (round (- (if steep-p x1 y1)
 						   (* slope (- (if steep-p y1 x1) p))))))
 		((= p (1+ (if steep-p y1 x1)))
 			(setf (.coordinates l) coords)
+			(when char (setf (.plot-char l) char))
 			l)
-		(setf coords (append coords (if steep-p (list q p) (list p q))))))
+		(setf coords (append coords (list (if steep-p (list q p) (list p q)))))))
+
+;;FIXME rewrite `line'
 
 (defun polygon (corners &key filled char)
 	"Return a polygon along a list of corners, optionally filled"
-	(do* ((pol (make-instance 'shape :char char))
+	(do* ((pol (make-instance 'shape))
 			 (i 0 (1+ i)) (j (1+ i) (1+ i)))
-		((= i (length corners)) (if filled (fill-shape pol) pol))
+		((= i (length corners))
+			(when char (setf (.plot-char pol) char))
+			(if filled (fill-shape pol) pol))
 		(when (= j (length corners)) (setf j 0))
 		(merge-shapes pol (line (first (nth i corners)) (second (nth i corners))
 							  (first (nth j corners)) (second (nth j corners)) char))))
@@ -129,4 +138,3 @@
 	"Return a circle with a given radius, optionally filled"
 	;;TODO
 	)
-
